9acdc45ebb2dd7a9b308edd3357d75746979e827
Natanel Niazoff <nniazoff@icloud.com>
2018-03-27 00:12:44 -0400

    Add array

---
+++ Array.swift
@@ -0,0 +1,15 @@
+//
+//  Array.swift
+//  Extensions
+//
+//  Created by Natanel Niazoff.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import Foundation
+
+public extension Array where Element: Equatable {
+    mutating func remove(_ element: Element) {
+        self = self.filter { $0 != element }
+    }
+}

f8d6a60ac82463157c97dfb6f510d9767414ccce
Natanel Niazoff <nniazoff@icloud.com>
2018-06-18 01:27:21 -0400

    Add UIViewController

---
+++ UIViewController.swift
@@ -0,0 +1,25 @@
+//
+//  UIViewController.swift
+//  Sammys
+//
+//  Created by Natanel Niazoff on 6/17/18.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import UIKit
+
+extension UIViewController {
+    func add(asChildViewController viewController: UIViewController) {
+        addChildViewController(viewController)
+        view.addSubview(viewController.view)
+        viewController.view.frame = view.bounds
+        viewController.view.autoresizingMask = [.flexibleWidth, .flexibleHeight]
+        viewController.didMove(toParentViewController: self)
+    }
+    
+    func remove(asChildViewController viewController: UIViewController) {
+        viewController.willMove(toParentViewController: nil)
+        viewController.view.removeFromSuperview()
+        viewController.removeFromParentViewController()
+    }
+}

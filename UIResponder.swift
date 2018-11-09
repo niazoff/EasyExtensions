b7b7f8f660dac41959c5eca7d7b54e8702eb6eb2
Natanel Niazoff <nniazoff@icloud.com>
2018-11-09 10:35:04 -0500

    Add next of certain type and use for UITableViewCell

---
+++ UIResponder.swift
@@ -0,0 +1,15 @@
+//
+//  UIResponder.swift
+//  EasyExtensions
+//
+//  Created by Natanel Niazoff on 11/8/18.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import UIKit
+
+extension UIResponder {
+	func next<T: UIResponder>(_ type: T.Type) -> T? {
+		return next as? T ?? next?.next(type)
+	}
+}
---
+++ UITableViewCell.swift
@@ -0,0 +1,19 @@
+//
+//  UITableViewCell.swift
+//  EasyExtensions
+//
+//  Created by Natanel Niazoff on 11/8/18.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import UIKit
+
+extension UITableViewCell {
+	var tableView: UITableView? {
+		return next(UITableView.self)
+	}
+	
+	var indexPath: IndexPath? {
+		return tableView?.indexPath(for: self)
+	}
+}

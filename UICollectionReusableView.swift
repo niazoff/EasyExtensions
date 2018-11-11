8901e89a6ef3c6c407d8bcbe7c4c29cda004858e
Natanel Niazoff <nniazoff@icloud.com>
2018-11-11 09:59:11 -0500

    Add UICollectionReusableView collectionView property

---
+++ UICollectionReusableView.swift
@@ -0,0 +1,15 @@
+//
+//  UICollectionReusableView.swift
+//  Sammys
+//
+//  Created by Natanel Niazoff on 11/10/18.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import UIKit
+
+public extension UICollectionReusableView {
+	var collectionView: UICollectionView? {
+		return next(UICollectionView.self)
+	}
+}

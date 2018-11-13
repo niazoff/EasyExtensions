0cb8446ad9aea6441272aca4cd263d50466809cb
Natanel Niazoff <nniazoff@icloud.com>
2018-11-12 19:06:39 -0500

    Add appending

---
+++ Array.swift
@@ -0,0 +1,17 @@
+//
+//  Array.swift
+//  Sammys
+//
+//  Created by Natanel Niazoff on 11/12/18.
+//  Copyright © 2018 Natanel Niazoff. All rights reserved.
+//
+
+import Foundation
+
+public extension Array {
+	func appending(_ element: Element) -> Array {
+		var array = self
+		array.append(element)
+		return array
+	}
+}

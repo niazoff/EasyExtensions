70a970d7c9913c12a58eebeac68d6a72de5a3fc7
Natanel Niazoff <nniazoff@icloud.com>
2019-04-02 14:25:15 -0400

    Add convenience target init for system item

--- UIBarButtonItem.swift
+++ UIBarButtonItem.swift
@@ -12,4 +12,8 @@ extension UIBarButtonItem {
     convenience init(image: UIImage?, style: UIBarButtonItem.Style, target: Target) {
         self.init(image: image, style: style, target: target, action: #selector(Target.performAction))
     }
+    
+    convenience init(barButtonSystemItem systemItem: UIBarButtonItem.SystemItem, target: Target) {
+        self.init(barButtonSystemItem: systemItem, target: target, action: #selector(Target.performAction))
+    }
 }

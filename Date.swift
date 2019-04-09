b659c212ee494f2848c4af97ab9a8fbf58d220a3
Natanel Niazoff <nniazoff@icloud.com>
2019-04-09 15:36:51 -0400

    Add roundedToNextQuarterHour

---
+++ Date.swift
@@ -0,0 +1,19 @@
+//
+//  Date.swift
+//  EasyExtensions
+//
+//  Created by Natanel Niazoff on 4/9/19.
+//  Copyright © 2019 Natanel Niazoff. All rights reserved.
+//
+
+import Foundation
+
+extension Date {
+    func roundedToNextQuarterHour(calendar: Calendar = .current) -> Date? {
+        let currentMinute = calendar.component(.minute, from: self)
+        let minuteAddend = (Int(Double(currentMinute/15).rounded(.down) + 1) * 15) - currentMinute
+        guard let minuteSumDate = calendar.date(byAdding: .minute, value: minuteAddend, to: self)
+            else { return nil }
+        return calendar.nextDate(after: minuteSumDate, matching: .init(second: 0), matchingPolicy: .nextTime, direction: .backward)
+    }
+}
